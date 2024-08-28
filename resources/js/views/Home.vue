9bf828029248519bc3ccfa4a6a164930031c5355
mohon sarker <mohonsarker40@gmail.com>
2024-08-28 16:11:04 +0600

    single page structure created

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -4,7 +4,8 @@
 
 <script>
     export default {
-        name: "Home.vue"
+        name: "Home.vue",
+        mounted(){}
     }
 </script>
 
--- resources/views/singleApp.blade.php
+++ resources/views/singleApp.blade.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel vue </title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     </head>
-    <body class="antialiased">
-    <div class="app">
-        
+    <body>
+    <div id="app">
+        <app/>
     </div>
+    
+    
+    <script src="{{asset('js/app.js')}}"></script>
+
     </body>
 </html>
