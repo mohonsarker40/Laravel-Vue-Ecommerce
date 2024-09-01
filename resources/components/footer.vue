54b44a729ab81e505921245ad7106c735550704b
mohon sarker <mohonsarker40@gmail.com>
2024-09-01 10:49:56 +0600

    sideNav, topNav and footer placed seperatly and app.js content shows in dashboard

---
+++ resources/components/footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer class="py-4 bg-light mt-auto">
+        <div class="container-fluid px-4">
+            <div class="d-flex align-items-center justify-content-between small">
+                <div class="text-muted">Copyright © Your Website 2023</div>
+                <div>
+                    <a href="#">Privacy Policy</a>
+                    ·
+                    <a href="#">Terms &amp; Conditions</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: "footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
